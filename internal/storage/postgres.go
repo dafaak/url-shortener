df6017f21af1b006c3ddebdda0a8e4fffaf160ab
Israel <israel.pe@outlook.es>
2026-01-12 12:54:44 -0500

    feat: postgres

---
+++ internal/storage/postgres.go
@@ -0,0 +1,27 @@
+package storage
+
+import (
+	"fmt"
+	"github.com/dafaak/url-shortener/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type PostgresStorage struct {
+	DB *gorm.DB
+}
+
+func NewPostgresStorage(dsn string) (*PostgresStorage, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("fallo al conectar a postgres: %w", err)
+	}
+
+	// Esto crea las tablas automáticamente si no existen
+	err = db.AutoMigrate(&models.URL{}, &models.Metric{})
+	if err != nil {
+		return nil, fmt.Errorf("error migrando tablas: %w", err)
+	}
+
+	return &PostgresStorage{DB: db}, nil
+}
