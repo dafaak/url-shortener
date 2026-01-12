b489f180ed2bb7bce5ec63c1e5acb2b70cfa65f3
Israel <israel.pe@outlook.es>
2026-01-12 16:16:14 -0500

    feat: get stats, get userUrls. refactor: record metric and shorten link

--- internal/handler/url_handler.go
+++ internal/handler/url_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/dafaak/url-shortener/internal/storage"
 	"github.com/dafaak/url-shortener/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/ua-parser/uap-go/uaparser"
 	"gorm.io/gorm"
 )
 
@@ -20,31 +22,127 @@ func NewURLHandler(db *storage.PostgresStorage, cache *storage.RedisStorage) *UR
 	return &URLHandler{DB: db, Cache: cache}
 }
 
-// Shorten crea el link corto
-func (h *URLHandler) Shorten(c *gin.Context) {
-	var req models.ShortenRequest
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "URL inválida"})
+func (h *URLHandler) GetStats(c *gin.Context) {
+	shortCode := c.Param("code")
+	//userID, _ := c.Get("userID")
+	userID := "dafaak"
+	var urlObj models.URL
+
+	// 1. Validar propiedad del link
+	if err := h.DB.DB.Where("short_code = ? AND user_id = ?", shortCode, userID).First(&urlObj).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Enlace no encontrado"})
 		return
 	}
 
-	code := utils.Encode(uint64(time.Now().UnixNano()))
+	stats := models.URLStats{
+		TotalClicks:  urlObj.ClickCount,
+		LastAccessed: urlObj.LastAccessedAt,
+		Browsers:     make(map[string]int),
+		OS:           make(map[string]int),
+		Platforms:    make(map[string]int),
+	}
+
+	// 2. Ejecutar conteos agrupados directamente en la DB
+	// Agrupar por Navegador
+	h.aggregateMetric(urlObj.ID, "browser", stats.Browsers)
+
+	// Agrupar por Sistema Operativo
+	h.aggregateMetric(urlObj.ID, "os", stats.OS)
+
+	// Agrupar por Plataforma (Mobile/Desktop)
+	h.aggregateMetric(urlObj.ID, "platform", stats.Platforms)
+
+	c.JSON(http.StatusOK, stats)
+}
+
+// Función auxiliar para evitar repetir código de agrupación
+func (h *URLHandler) aggregateMetric(urlID uint, column string, targetMap map[string]int) {
+	rows, err := h.DB.DB.Model(&models.Metric{}).
+		Select(column+" as label, count(*) as total").
+		Where("url_id = ?", urlID).
+		Group(column).
+		Rows()
+
+	if err == nil {
+		defer rows.Close()
+		for rows.Next() {
+			var label string
+			var total int
+			rows.Scan(&label, &total)
+			if label == "" {
+				label = "Unknown"
+			}
+			targetMap[label] = total
+		}
+	}
+}
+
+func (h *URLHandler) GetUserURLs(c *gin.Context) {
+	userID := c.Param("userId")
+	var urls []models.URL
+
+	// Buscamos en la base de datos filtrando por user_id
+	result := h.DB.DB.Where("user_id = ?", userID).Find(&urls)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al obtener los enlaces"})
+		return
+	}
+
+	c.JSON(http.StatusOK, urls)
+}
+
+// Shorten crea el link corto
+func (h *URLHandler) Shorten(c *gin.Context) {
+	var req models.ShortenRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Datos inválidos"})
+		return
+	}
+
+	var finalCode string
+	fmt.Println("custom_code", req.CustomCode)
+	if req.CustomCode != "" {
+
+		// --- CASO ALIAS PERSONALIZADO ---
+		// 1. Validar longitud o caracteres (opcional)
+		if len(req.CustomCode) < 3 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "El alias debe tener al menos 3 caracteres"})
+			return
+		}
+
+		// 2. Verificar si ya existe en la base de datos
+		var exists int64
+		h.DB.DB.Model(&models.URL{}).Where("short_code = ?", req.CustomCode).Count(&exists)
+		if exists > 0 {
+			c.JSON(http.StatusConflict, gin.H{"error": "Este alias ya está en uso"})
+			return
+		}
+		finalCode = req.CustomCode
+	} else {
+		// --- CASO ALEATORIO ---
+		finalCode = utils.Encode(uint64(time.Now().UnixNano()))
+	}
+
 	urlObj := models.URL{
 		OriginalURL: req.URL,
-		ShortCode:   code,
+		ShortCode:   finalCode,
 		UserID:      req.UserID,
 		ExpiresAt:   req.ExpiresAt,
 	}
 
 	if err := h.DB.DB.Create(&urlObj).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al guardar"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo crear el link"})
 		return
 	}
 
-	// Guardar en caché por 24h
-	h.Cache.Cli.Set(c.Request.Context(), code, urlObj.OriginalURL, 24*time.Hour)
+	// Guardar en caché
+	h.Cache.Cli.Set(c.Request.Context(), finalCode, urlObj.OriginalURL, 24*time.Hour)
 
-	c.JSON(http.StatusCreated, gin.H{"short_url": "http://localhost:8080/" + code})
+	c.JSON(http.StatusCreated, gin.H{
+		"short_url": "http://localhost:8080/" + finalCode,
+		"code":      finalCode,
+	})
 }
 
 // Redirect busca y redirige
@@ -75,9 +173,29 @@ func (h *URLHandler) Redirect(c *gin.Context) {
 }
 
 func (h *URLHandler) recordMetric(code string, c *gin.Context) {
-	// Aquí guardarías en models.Metric usando h.DB.DB
-	// Por ahora solo actualizamos el contador global
-	h.DB.DB.Model(&models.URL{}).Where("short_code = ?", code).
-		UpdateColumn("click_count", gorm.Expr("click_count + 1")).
-		UpdateColumn("last_accessed_at", time.Now())
+	// 1. Obtenemos el ID de la URL
+	var urlObj models.URL
+	if err := h.DB.DB.Select("id").Where("short_code = ?", code).First(&urlObj).Error; err != nil {
+		return
+	}
+
+	// 2. Parsear el User-Agent
+	uaString := c.Request.UserAgent()
+	parser := uaparser.NewFromSaved()
+	client := parser.Parse(uaString)
+
+	// 3. Crear el registro de métrica
+	metric := models.Metric{
+		URLID:     urlObj.ID,
+		IPAddress: c.ClientIP(),
+		Browser:   client.UserAgent.Family,
+		OS:        client.Os.Family,
+		Platform:  client.Device.Family,
+		Referrer:  c.Request.Referer(),
+	}
+
+	// 4. Guardar en DB y actualizar contador global
+	h.DB.DB.Create(&metric)
+	h.DB.DB.Model(&urlObj).UpdateColumn("click_count", gorm.Expr("click_count + 1"))
+	h.DB.DB.Model(&urlObj).UpdateColumn("last_accessed_at", time.Now())
 }
