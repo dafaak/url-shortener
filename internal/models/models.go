6673ec5fc5a84635cd0dacc9cd5fa9b93963737f
Israel <israel.pe@outlook.es>
2026-01-14 13:08:26 -0500

    feat: models needed for auth

--- internal/models/models.go
+++ internal/models/models.go
@@ -7,13 +7,14 @@ import (
 // URL es la tabla principal
 type URL struct {
 	ID             uint       `gorm:"primaryKey" json:"id"`
-	UserID         *string    `gorm:"index" json:"user_id,omitempty"`
+	Username       *string    `gorm:"index" json:"username,omitempty"`
 	OriginalURL    string     `gorm:"not null" json:"original_url"`
 	ShortCode      string     `gorm:"uniqueIndex;not null;size:15" json:"short_code"`
 	CreatedAt      time.Time  `json:"created_at"`
 	LastAccessedAt *time.Time `json:"last_accessed_at,omitempty"`
 	ExpiresAt      *time.Time `gorm:"index" json:"expires_at,omitempty"`
 	ClickCount     int        `gorm:"default:0" json:"click_count"`
+	Metrics        []Metric   `gorm:"foreignKey:URLID" json:"-"`
 }
 
 // Metric es la tabla de analíticas
@@ -29,6 +30,14 @@ type Metric struct {
 	Platform    string    `gorm:"size:50" json:"platform"`
 }
 
+type User struct {
+	ID       uint   `gorm:"primaryKey" json:"id"`
+	Email    string `gorm:"uniqueIndex;not null" json:"email"`
+	Username string `gorm:"uniqueIndex;not null" json:"username"`
+	Password string `gorm:"not null" json:"-"` // "-" para que nunca viaje en el JSON
+	URLs     []URL  `gorm:"foreignKey:Username;references:Username" json:"urls,omitempty"`
+}
+
 type URLStats struct {
 	TotalClicks  int            `json:"total_clicks"`
 	LastAccessed *time.Time     `json:"last_accessed"`
@@ -41,6 +50,16 @@ type URLStats struct {
 type ShortenRequest struct {
 	URL        string     `json:"url" binding:"required,url"`
 	CustomCode string     `json:"custom_code,omitempty"`
-	UserID     *string    `json:"user_id,omitempty"`
+	Username   *string    `json:"username,omitempty"`
 	ExpiresAt  *time.Time `json:"expires_at"`
+}
+
+type RegisterRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Username string `json:"username" binding:"required,min=3,max=20"` // Obligatorio y con límites
+	Password string `json:"password" binding:"required,min=6"`
+}
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
 }
