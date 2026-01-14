864d4ee96c095fc63a305c831ecea2fdbac5ad46
Israel <israel.pe@outlook.es>
2026-01-14 13:10:05 -0500

    refactor:  separete public endpoints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,13 +25,30 @@ func main() {
 	}
 
 	// 2. Handler y Rutas
+	authH := &handler.AuthHandler{DB: pg}
 	h := handler.NewURLHandler(pg, rd)
 	r := gin.Default()
 
-	r.POST("/shorten", h.Shorten)
+	// Auth público
+	r.POST("/register", authH.Register)
+	r.POST("/login", authH.Login)
+
+	// Redirección pública
 	r.GET("/:code", h.Redirect)
-	r.GET("/user/:userId/links", h.GetUserURLs)
-	r.GET("/stats/:code", h.GetStats)
+	//r.GET("/:user/:code", h.Redirect)
+
+	// Lista de links pública
+	r.GET("/user/:username/links", h.GetUserURLs)
+
+	//shorten público
+	r.POST("/shorten", h.Shorten)
+
+	// Rutas protegidas
+	api := r.Group("/api")
+	api.Use(handler.AuthMiddleware())
+	{
+		r.GET("/stats/:code", h.GetStats)
+	}
 
 	log.Println("Servidor corriendo en :8080")
 	r.Run(":8080")
