b64f34f36561fd0cc0cf107c4426b31909034eb9
dafaak <israel.pe@outlook.es>
2026-05-01 14:41:54 -0500

    refactor: new fb ips added as bots

--- utils/is_bot.go
+++ utils/is_bot.go
@@ -5,38 +5,53 @@ import (
 	"strings"
 )
 
-func IsBot(ua string, ipStr string) bool {
-	lowUA := strings.ToLower(ua)
-	
-	
-	botKeywords := []string{
-		"bot", "spider", "crawler", "facebookexternalhit", 
-		"linkedinbot", "twitterbot", "whatsapp", "telegrambot",
+// Variable global para almacenar los rangos ya procesados en memoria
+var compiledNetworks []*net.IPNet
+
+// init se ejecuta automáticamente una sola vez al levantar el servidor
+func init() {
+	rawNetworks := []string{
+		"173.252.64.0/18", // Meta
+		"31.13.64.0/18",   // Meta
+		"57.141.0.0/16",   // Meta (Aquí están las IPs 57.141.6.x que se filtraron)
+		"108.174.0.0/20",  // LinkedIn
+		"199.16.156.0/22", // X
+		"34.192.0.0/10",   // AWS
+		"54.144.0.0/12",   // AWS
 	}
+
+	for _, cidr := range rawNetworks {
+		_, network, err := net.ParseCIDR(cidr)
+		if err == nil {
+			compiledNetworks = append(compiledNetworks, network)
+		}
+	}
+}
+
+// IsBot evalúa si una petición viene de un humano o un script
+func IsBot(ua string, ipStr string) bool {
+	// 1. Verificación por User-Agent (Keywords)
+	lowUA := strings.ToLower(ua)
+	botKeywords := []string{
+		"bot", "spider", "crawler", "facebookexternalhit",
+		"twitterbot", "linkedinbot", "whatsapp", "telegrambot",
+	}
+
 	for _, keyword := range botKeywords {
 		if strings.Contains(lowUA, keyword) {
 			return true
 		}
 	}
 
-	
+	// 2. Verificación por IP (Rangos de Datacenters)
 	ip := net.ParseIP(ipStr)
 	if ip == nil {
-		return true 
+		return true // IP inválida = Petición sospechosa
 	}
 
-	// Lista de rangos conocidos de Datacenters/Redes Sociales
-	networks := []string{
-		"173.252.64.0/18",  // Facebook / Instagram
-		"31.13.64.0/18",    // Facebook
-		"108.174.0.0/20",   // LinkedIn
-		"199.16.156.0/22",  // Twitter
-		"34.192.0.0/10",    // AWS (Cubre muchos crawlers genéricos)
-	}
-
-	for _, cidr := range networks {
-		_, network, err := net.ParseCIDR(cidr)
-		if err == nil && network.Contains(ip) {
+	// Iteramos sobre los rangos pre-compilados (muy rápido)
+	for _, network := range compiledNetworks {
+		if network.Contains(ip) {
 			return true
 		}
 	}
